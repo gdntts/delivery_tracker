c9a762c6231e36b7528f54c228edf49d0f772c30
gdntts <gustavo.dantas.marim@gmail.com>
2026-06-22 11:05:21 -0300

    feat(listener): implementa listener de notificações de localização
    - Configurações de cors para possibilitar os testes em arquivo html
    - Bean do TaskExecutor dentro de WebSocketConfig
    - DTO de LocationEvent
    - Componente LocationNotificationListener para ouvir o pool de notificações

--- src/main/java/dev/gustavodntts/deliverytracker/config/WebSocketConfig.java
+++ src/main/java/dev/gustavodntts/deliverytracker/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package dev.gustavodntts.deliverytracker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -18,6 +21,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Bean(name = "taskExecutor")
+    public TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
     }
 }
